0ba2955bb304cec0deb47dcfcb4e9d8180533587
Prashanth Puranik <puranik@digdeeper.in>
2023-02-28 23:11:38 +0530

    str ex 3

--- src/string/NumberOfVowelsAndConsonants.java
+++ src/string/NumberOfVowelsAndConsonants.java
@@ -3,7 +3,23 @@ package string;
 public class NumberOfVowelsAndConsonants {
     // for str -> "Hello world", it prints 3 Vowels ('a', 'e', 'i', 'o', 'u') and 7 consonants
     private static void printNumVowelsAndConsonants( String str ) {
+        str = str.toLowerCase();
+        int vCount = 0, cCount = 0;
 
+        for( int i = 0; i < str.length(); i++ ) {
+            char cur = str.charAt( i );
+
+            if( cur == 'a' || cur == 'e' || cur == 'i' || cur == 'o' || cur == 'u' ) {
+                vCount++;
+            } else {
+                if( cur != ' ' ) {
+                    cCount++;
+                }
+            }
+        }
+
+        System.out.println( "vowel count is " + vCount );
+        System.out.println( "consonant count is " + cCount );
     }
 
     public static void main(String[] args) {
---
+++ src/string/Palindrome.java
@@ -0,0 +1,16 @@
+package string;
+
+public class Palindrome {
+    private static boolean palindrome( String str ) {
+        // 5 minutes for trying
+        
+    }
+
+    public static void main(String[] args) {
+        String str1 = "madam";
+        System.out.println( palindrome( str1 ) ); // true
+
+        String str2 = "adam";
+        System.out.println( palindrome( str2 ) ); // false
+    }
+}
